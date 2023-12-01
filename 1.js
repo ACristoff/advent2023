cc53d16259f3369c154333f4b88a01256c9c4792
Alex Cristoffanini <acristoffanini@gmail.com>
2023-12-01 13:01:32 -0800

    first half of day 1 puzzle, a function that returns two digit coordinates for a trebuchet

--- 1.js
+++ 1.js
@@ -1,4 +1,23 @@
+const fs = require('fs')
+const readline = require("readline");
 
+const lineReader = readline.createInterface({
+    input: fs.createReadStream('input1.txt'),
+    output: process.stdout,
+    terminal: false
+})
 
+const numRegex = /\d/g
 
-console.log('test')
+const returnCoordinate = (line) => {
+    const cleanLine = line.match(numRegex)
+    const coordinates = cleanLine[0] + cleanLine[cleanLine.length -1]
+    return coordinates
+}
+
+let sum = 0
+
+lineReader.on('line', (line) => {
+    const coords = parseInt(returnCoordinate(line))
+    // sum += coords
+})
