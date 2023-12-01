830b5dd9512b8ba86a6023406443a7deafff7586
Alex Cristoffanini <acristoffanini@gmail.com>
2023-12-01 15:56:48 -0800

    modified function that searches for both the first and last numbers in a string

--- 1.js
+++ 1.js
@@ -7,11 +7,37 @@ const lineReader = readline.createInterface({
     terminal: false
 })
 
-const numRegex = /\d/g
+const numberMap = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+const verifyNumber = (string) => {
+    if (!parseInt(string)) {
+        return numberMap[string]
+    }
+    return parseInt(string)
+}
+
+const numRegex = /(?:[0-9]|one|two|three|four|five|six|seven|eight|nine)/gi;
+const revNumRegex = /(?:[0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/gi;
+
+const reverse = (str) => {
+    return str.split("").reverse().join("")
+}
 
 const returnCoordinate = (line) => {
-    const cleanLine = line.match(numRegex)
-    const coordinates = cleanLine[0] + cleanLine[cleanLine.length -1]
+    const firstMatch = line.match(numRegex)[0]
+    const lastMatch = reverse(reverse(line).match(revNumRegex)[0])
+    
+    const coordinates = `${verifyNumber(firstMatch)}${verifyNumber(lastMatch)}`
     return coordinates
 }
 
@@ -19,5 +45,9 @@ let sum = 0
 
 lineReader.on('line', (line) => {
     const coords = parseInt(returnCoordinate(line))
-    // sum += coords
+    sum += coords
+})
+
+lineReader.on('close', () => {
+    console.log(sum)
 })
