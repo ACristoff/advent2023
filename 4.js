ce7af0f8aee0fc18fe133eb8609c2f6bd480a4b1
Alex Cristoffanini <acristoffanini@gmail.com>
2023-12-06 14:22:41 -0800

    pseudocode planning

--- 4.js
+++ 4.js
@@ -9,8 +9,18 @@ const lineReader = readline.createInterface({
 
 let sum = 0
 
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+
+//For each card
+//Seperate card numbers and winning numbers into two sets
+//Loop over winning numbers and search card number for matches
+//For each match, push to an array
+//For each entry in the array multiply the score by 2 (base 1 if there is at least one match)
+
+//Sum results
+
 lineReader.on('line', (line) => {   
-    matrix.push(line)
+    console.log(line)
 })
 
 
