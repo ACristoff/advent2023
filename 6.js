f8797e9a7745a5a2aeeb05f455b3b4085d3175f6
Alex Cristoffanini <acristoffanini@gmail.com>
2023-12-14 00:43:02 -0800

    initial planning

--- 6.js
+++ 6.js
@@ -1,17 +1,22 @@
 const fs = require('fs')
-const readline = require("readline");
+const path = require("path")
+const input = fs.readFileSync(path.resolve(__dirname, "input6.txt"), "utf8").split("\n")
 
-const lineReader = readline.createInterface({
-    input: fs.createReadStream('input5.txt'),
-    output: process.stdout,
-    terminal: false
+const distances = input[0].split(' ').filter(function(str) { return /\S/.test(str); })
+const times = input[1].split(' ').filter(function(str) { return /\S/.test(str); })
+
+const races = distances.slice(1, distances.length).map((entry, index) => {
+    return [entry, times[index + 1]]
 })
 
-lineReader.on('line', (line) => {
+//Form a distance and time pairing, push it to the races array
 
-})
+// This document describes three races:
+// The first race lasts 7 milliseconds. The record distance in this race is 9 millimeters.
+// The second race lasts 15 milliseconds. The record distance in this race is 40 millimeters.
+// The third race lasts 30 milliseconds. The record distance in this race is 200 millimeters.
+
+// Your toy boat has a starting speed of zero millimeters per millisecond. For each whole millisecond you spend at the beginning of the race holding down the button, the boat's speed increases by one millimeter per millisecond.
 
 
-lineReader.on('close', () => {
-    console.log('---End Log---')
-})
+console.log(distances, times, races)
